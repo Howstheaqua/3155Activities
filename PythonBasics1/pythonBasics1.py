838aaa3390e5b669c08c736f7f31d91d782c211f
Evan Hughes <38701438+Howstheaqua@users.noreply.github.com>
2022-09-18 22:53:56 -0400

    added logic to fizz_buzz

---
+++ PythonBasics1/pythonBasics1.py
@@ -0,0 +1,50 @@
+# Python Activity
+#
+# Fill in the code for the functions below.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code. Make sure to add what is going to be returned.
+
+# Part A. odd_range
+# Define a function odd_range(num1, num2) that takes a starting number (num1) and an ending number (num2)
+# and returns all odd numbers as an array between num1 (inclusive) and num2 (exclusive)
+def odd_range(num1, num2):
+  oddnums = []
+  for i in range(num1,num2):
+    if (i%2!=0):
+      oddnums.append(i)
+
+  return oddnums
+
+# Part B. has_lower_case
+# Define a function has_lower_case(s) that takes a string s
+# and checks if string s contains a lower case char.
+# The function should return True indicating that string s has a lower case char
+# otherwise return False
+def has_lower_case(s):
+  # YOUR CODE HERE
+  has_lower_case_bool = False
+  for char in s:
+    if char.islower():
+      has_lower_case_bool = True
+  return has_lower_case_bool
+
+# Part C. fizz_buzz
+# Define a function fizz_buzz(num) that takes an integer num
+# and returns a string based on the following criteria:
+
+# if num is divisible by 3 return "Fizz"
+# if num is divisible by 5 return "Buzz"
+# if num is divisible by 3 and 5 return "FizzBuzz"
+
+# if num is does not meet any of the above criteria or is less than
+# or equal to 0 return the num as a string
+def fizz_buzz(num):
+  # YOUR CODE HERE
+  output = ''
+  if num%3==0:
+    output += "Fizz"
+  if num%5==0:
+    output += "Buzz"
+  if output == '' or num <= 0:
+    output = str(num)
+  return output
