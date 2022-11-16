699851a55c092a1ee115d778bbfb17ce9fee6f8f
Howstheaqua <hughes.e.47201@gmail.com>
2022-11-16 18:42:55 -0500

    fixed detached head

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -9,7 +9,8 @@ from flask import request, redirect, url_for
 from database import db
 from models import Note as Note
 from models import User as User
-from forms import RegisterForm, LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 from flask import session
 
 app = Flask(__name__)     # create an app
@@ -45,9 +46,14 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user= db.session.query(User).filter_by(email='ehughe29@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('note.html', note=my_note, user=a_user)
+    if session.get('user'):
+
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        form = CommentForm()
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -142,6 +148,24 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
+
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
